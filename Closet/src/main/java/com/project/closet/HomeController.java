0c41b3e65273981f6d2a7711d210e82b7b83d355
victo <victo@221.139.43.205>
2018-09-06 08:06:36 +0900

    Copy Project

--- Closet/src/main/java/com/project/closet/HomeController.java
+++ Closet/src/main/java/com/project/closet/HomeController.java
@@ -1,39 +1,49 @@
 package com.project.closet;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Handles requests for the application home page.
+/* 
+ * Developer : G
+ * Description : 프로젝트의 전반적인 흐름에 대한 명시.
+ * FLOW
+ * = View(JSP).submit(Form데이터 제출에 대하여)
+ *   -> @Controller에 해당하는class에서 ReqeustMapping의 값을 추적
+ *   -> Service(Impl)에서 1차적으로 데이터 가공, 혹은 Dao(Impl)에서 가져온 값을 가공.
+ *   -> Dao(Impl)에서 SqlSession으로 쿼리호출(데이터 직접 접근) 및 Return값 GET.
+ *   -> Dao(Impl)에서 Service(Impl)로 값 전달, Service에서는 값에 따른 True X False 확인 후 Controller로 전달
+ *   -> Controller 에서 Service로부터 확인된 값에 따라 View에 값 전달(ModelAndView) 또는 True X False 로직 수행.
+ *   -> 화면 전환.
+ * Flow 요약 : View -> Controller -> Service -> Dao -> Database(MyBatis) - > Dao(R) -> Service(R) -> Controller(R) -> View(R)			// R = Result
+ * Date : 2018.09.04
+ * E.T.C : HomeController.java는 절대 지우지 말 것, BasePackage파일이어서 그런지, 지우면 다른것도 다 먹통 됨.
  */
+
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
 		return "main";
 	}
+	
+	@RequestMapping("sessionEndUp")
+	public ModelAndView sessionEndUp() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("msg","세션이 만료되었습니다. 다시 로그인해주세요.");
+		mav.setViewName("main");
+		
+		return mav;
+	}
+	
+//	@RequestMapping("/about")
+//	public String about() {
+//		return "about/about";
+//	}
 	
 }
--- Closet/src/main/webapp/WEB-INF/jsp/main.jsp
+++ Closet/src/main/webapp/WEB-INF/jsp/main.jsp
@@ -1,14 +1,64 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
 <%@ page session="false" %>
-<html>
-<head>
-	<title>Home</title>
-</head>
-<body>
-<h1>
-	Hello world!  
-</h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+<%@ include file="/WEB-INF/jsp/common/head.jsp"%>
+<script>
+	$(document).ready(function() {
+
+	});
+</script>
+<body>
+<div class="closetBoard">
+	<div class="closetView" id="closet">
+		<!-- .html()메소드로 객체 호출. -->
+		ClosetImageView
+	</div>
+	<div class="liking">
+		<span class="clover"></span>
+		<span class="cloverPoint" id="cCount">0</span>
+		<span class="flover"></span>
+		<span class="floverPoint" id="fCount">0</span>
+	</div>
+	<div class="closetInfo">
+		<ul class="userInfo">
+			<li id="userPhoto" style="background:url('/file/img/ssu.png') no-repeat 50%;background-size:100%;"></li>
+			<li id="regDate">2018-MM-DD(Mon), 24:mm:ss</li>
+			<li id="userName">작성자</li>
+			<li id="boardTitle">제목</li>
+		</ul>
+	</div>
+</div>
+<div class="event">
+	<div class="eventView">
+		<div class="eventImg">
+			Ad Poster
+		</div>
+		<p class="eventTitle">
+			Ad Title
+		</p>
+	</div>
+</div>
+<div class="promition">
+	<div class="promitionView">
+		<div class="promitionImg">
+			Pm Poster
+		</div>
+		<p class="promitionTitle">
+			Pm Title
+		</p>
+	</div>
+</div>
+<div class="testBox" style="display:none;">
+	<h1>TEST ZONE</h1>
+	<div id="testZone">
+		테스트존이다.
+	</div>
+</div>
 </body>
+<%@ include file="/WEB-INF/jsp/layer/layerSimple.jsp"%>
+<%@ include file="/WEB-INF/jsp/common/bottom.jsp"%>
 </html>
